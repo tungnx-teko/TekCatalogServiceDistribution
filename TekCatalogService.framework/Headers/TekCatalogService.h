324d20f687965dc23bd0ebb1645edff501de2724
Tung Nguyen <xtungit@gmail.com>
2020-08-11 17:06:50 +0700

    Init

---
+++ TekCatalogService.framework/Headers/TekCatalogService.h
@@ -0,0 +1,19 @@
+//
+//  TekCatalogService.h
+//  TekCatalogService
+//
+//  Created by Tung Nguyen on 8/11/20.
+//  Copyright © 2020 Tung Nguyen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TekCatalogService.
+FOUNDATION_EXPORT double TekCatalogServiceVersionNumber;
+
+//! Project version string for TekCatalogService.
+FOUNDATION_EXPORT const unsigned char TekCatalogServiceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TekCatalogService/PublicHeader.h>
+
+
